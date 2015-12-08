9c69f1c333a8e33a4352a0c6740ebf564e1cbb65
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2015-12-08 15:30:54 +0100

    linkID WS Login Example, basic js polling

--- linkid-example/linkid-example/LoginWS.aspx
+++ linkid-example/linkid-example/LoginWS.aspx
@@ -4,7 +4,56 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml" >
 <head runat="server">
-    <title></title>
+    <title>linkID Login Example</title>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript">
+        function pollLinkID() {
+        
+            (function poll() {
+
+                console.log("poll...");
+
+                $.ajax({
+                    type: "POST",
+                    contentType: "application/json; charset=utf-8",
+                    url: "LoginWS.aspx/pollLinkID",
+                    data: null,
+                    dataType: "json",
+                    success: function(result) {
+                        var jsonData = $.parseJSON(result.d);
+                        console.log("Info: " + jsonData.info);
+                        if (!jsonData.finished) {
+                            setTimeout("pollLinkID()", 2000);
+                        } 
+
+                    },
+
+                    error: function(result) {
+                        alert("Error");
+                    }
+
+                });
+
+                /*
+                var pollResult = PageMethods.pollLinkID();
+                console.log("poll result" . pollResult);
+                if (pollResult.hideQR) {
+                console.log("hide QR...");
+                } else {
+                console.log("show QR...");
+                }
+                setTimeout("pollLinkID()", 2000);
+                */
+            })();
+        
+        };
+    </script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            pollLinkID();
+            console.log("document loaded");
+        });
+    </script>
 </head>
 <body>
     <div>
@@ -16,6 +65,7 @@
         </pre>
 
         <form id="form1" runat="server">
+            <asp:ScriptManager ID='ScriptManager1' runat='server' EnablePageMethods='true' />        
             <asp:LinkButton ID="restart" runat="server" Text="Restart" OnClick="onRestart" />        
         </form>
     </div>
--- linkid-example/linkid-example/LoginWS.aspx.cs
+++ linkid-example/linkid-example/LoginWS.aspx.cs
@@ -5,6 +5,8 @@ using System.Net.Security;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Services;
+using System.Web.Script.Serialization;
 using safe_online_sdk_dotnet;
 
 namespace linkid_example
@@ -15,12 +17,35 @@ namespace linkid_example
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            PollResult result = doLinkIDPoll();
+            if (!result.hideQR)
+            {
+                qr.Src = "data:image/png;base64," + result.encodedQR;
+                qr.Visible = true;
+            }
+            else
+            {
+                state.Text = result.info;
+                qr.Visible = false;
+            }
+        }
 
+        [WebMethod(EnableSession=true)]
+        public static string pollLinkID()
+        {
+            PollResult result = doLinkIDPoll();
+            return new JavaScriptSerializer().Serialize(result);
+
+        }
+
+        private static PollResult doLinkIDPoll()
+        {
             // Allow any SSL certficate ( ONLY FOR DEVELOPMENT!! )
             ServicePointManager.ServerCertificateValidationCallback =
                 new RemoteCertificateValidationCallback(WCFUtil.AnyCertificateValidationCallback);
 
-            LinkIDAuthSession linkIDSession = (LinkIDAuthSession)Session[LINKID_SESSION];
+            LinkIDAuthSession linkIDSession = (LinkIDAuthSession)HttpContext.Current.Session[LINKID_SESSION];
+            PollResult result;
 
             if (null == linkIDSession)
             {
@@ -31,7 +56,7 @@ namespace linkid_example
                 linkIDContext.applicationName = TestUtil.APP_NAME;
                 linkIDContext.language = TestUtil.language;
                 linkIDContext.identityProfile = "linkid_basic";
-                
+
                 // attribute suggestions
                 Dictionary<string, List<Object>> attributeSuggestions = new Dictionary<string, List<object>>();
                 attributeSuggestions.Add("test.attribute.string", new List<Object> { "test" });
@@ -45,31 +70,35 @@ namespace linkid_example
                 // linkIDContext.callback = new LinkIDCallback("https://www.google.be", null, true);
 
                 // start the linkID authentication
-                linkIDSession = TestUtil.getClient().authStart(linkIDContext, Request.UserAgent);
+                linkIDSession = TestUtil.getClient().authStart(linkIDContext, HttpContext.Current.Request.UserAgent);
 
-                qr.Src = "data:image/png;base64," + linkIDSession.qrCodeInfo.qrEncoded;
+                //qr.Src = "data:image/png;base64," + linkIDSession.qrCodeInfo.qrEncoded;
 
-                Session[LINKID_SESSION] = linkIDSession;
+                HttpContext.Current.Session[LINKID_SESSION] = linkIDSession;
+
+                result = new PollResult(false, linkIDSession.qrCodeInfo.qrEncoded, false, null);
             }
             else
             {
                 // poll linkID authentication
                 LinkIDAuthPollResponse pollResponse = TestUtil.getClient().authPoll(linkIDSession.sessionId, TestUtil.language);
 
-                state.Text = "<h2>Poll response</h2>";
-                state.Text += "AuthenticationState: " + pollResponse.linkIDAuthenticationState + "<br/>";
-                state.Text += "PaymentState: " + pollResponse.paymentState + "<br/>";
-                state.Text += "Payment Menu URL: " + pollResponse.paymentMenuURL + "<br/>";
+                String info = "";
+                info = "<h2>Poll response</h2>";
+                info += "AuthenticationState: " + pollResponse.linkIDAuthenticationState + "<br/>";
+                info += "PaymentState: " + pollResponse.paymentState + "<br/>";
+                info += "Payment Menu URL: " + pollResponse.paymentMenuURL + "<br/>";
 
                 if (null != pollResponse.linkIDAuthnResponse)
                 {
                     // logged in, dump user data
-                    state.Text += pollResponse.linkIDAuthnResponse.ToString();
+                    info += pollResponse.linkIDAuthnResponse.ToString();
                 }
 
-                // hide qr image
-                qr.Visible = false;
+                result = new PollResult(true, null, null != pollResponse.linkIDAuthnResponse, info);
             }
+
+            return result;
         }
 
         protected void onRestart(Object sender, EventArgs e)
@@ -77,6 +106,22 @@ namespace linkid_example
             Session[LINKID_SESSION] = null;
             Session.Abandon();
             Page.Response.Redirect(Page.Request.Url.ToString(), true);
+        }
+
+        public class PollResult
+        {
+            public bool hideQR { get; set; }
+            public String encodedQR { get; set; }
+            public bool finished { get; set; }
+            public String info { get; set; }
+
+            public PollResult(bool hideQR, String encodedQR, bool finished, String info)
+            {
+                this.hideQR = hideQR;
+                this.encodedQR = encodedQR;
+                this.finished = finished;
+                this.info = info;
+            }
         }
     }
 }
